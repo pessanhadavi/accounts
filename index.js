7062591b2ebd807353f24df9ae91deea48cc1988
David Pessanha <davidppessanha@gmail.com>
2021-12-15 19:42:29 -0300

    Init system

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
 // modulos externos
-const inquirer = require("inquirer")
-const chalk = require("chalk")
+const inquirer = require("inquirer");
+const chalk = require("chalk");
 
 // modulos internos
-const fs = require("fs")
+const fs = require("fs");
 
-console.log("iniciamos o Accounts")
+const operation = () => {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "action",
+        message: "O que você deseja fazer? ",
+        choices: [
+          "Criar Conta",
+          "Consultar Saldo",
+          "Depositar",
+          "Sacar",
+          "Sair"
+        ]
+      }
+    ])
+    .then((answer) => {
+      const action = answer["action"]
+
+      console.log(action)
+    })
+    .catch((err) => console.log(err));
+};
+
+operation()
